e3e2f09c3ff6c2731e530a06916b9551a4bc54d7
Kayvon Rahimi Morad Ali <krahimim@avans.nl>
2017-05-03 20:28:23 +0200

    routes added

---
+++ routes/routes_api_v2.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Kayvon Rahimi on 3-5-2017.
+ */
+// API version 2
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+router.get('/info', function(request, response) {
+    response.status(200);
+    response.json({
+        "description": "Recipes REST server API version 2."
+    });
+});
+
+router.get('*', function(request, response){
+   response.status(404);
+   response.send("404 - Response not found " + '\n'
+       + "Wrong parameter, please check your URL again.")
+});
+
+module.exports = router;
